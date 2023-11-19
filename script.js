0a65d04b6b7f24c06455b79ee7e80c5c2e4c33ac
Diego C. <diegodiazdelaconcha93@gmail.com>
2023-11-19 02:30:19 -0600

    pestañas completas de preferencias y perfil

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
-// VARIABLES
+// VARIABLES para la pesta;a de preferencias 
 const PreferenciasText = document.querySelector('.preference');
 const PreferenceTab = document.querySelector('.div-preference');
+// Variables para la pesta;a de las cuentas o perfil
+const CuentaText = document.querySelector('.Profile');
+const CuentaTab = document.querySelector('.Tab-account')
 
 
-// EVENTS
+// EVENT de pesta;a preferencias
 PreferenciasText.addEventListener('click', tabPreference);
-
+//Event de pesta;a Perfil
+CuentaText.addEventListener('click', tabCuenta);
 
 function tabPreference() {
-    if(PreferenceTab.classList.contains('inactive')){
-        PreferenceTab.classList.remove('inactive');
-    }else{
-        PreferenceTab.classList.add('inactive');
+    const isClosed = CuentaTab.classList.contains('inactive');
+    if(!isClosed){
+        CuentaTab.classList.add('inactive');
     }
+    PreferenceTab.classList.toggle('inactive');
 }
 
+function tabCuenta() {
+    const isClosed = PreferenceTab.classList.contains('inactive');
+    if(!isClosed){
+        PreferenceTab.classList.add('inactive');
+    }
+
+    CuentaTab.classList.toggle('inactive');
+    
+}
 
