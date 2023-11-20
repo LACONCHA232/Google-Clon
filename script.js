e136a5b6c7a3f31f6163b84f5b71a24ae3895744
Diego C. <diegodiazdelaconcha93@gmail.com>
2023-11-19 18:28:21 -0600

    update del tab perfiles, para añadir mas perfiles mediante JS

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
 
 // VARIABLES para la pesta;a de preferencias 
-const PreferenciasText = document.querySelector('.preference');
-const PreferenceTab = document.querySelector('.div-preference');
+const $PreferenciasText = document.querySelector('.preference');
+const $PreferenceTab = document.querySelector('.div-preference');
 // Variables para la pesta;a de las cuentas o perfil
-const CuentaText = document.querySelector('.Profile');
-const CuentaTab = document.querySelector('.Tab-account')
+const $CuentaText = document.querySelector('.Profile');
+const $CuentaTab = document.querySelector('.Tab-account')
+const $Cuentas = document.querySelector('.accounts');
+const $ShowAccounts = document.querySelector('.show-accounts');
+const $Accounts =document.querySelector('.accounts');
 
 
-// EVENT de pesta;a preferencias
-PreferenciasText.addEventListener('click', tabPreference);
-//Event de pesta;a Perfil
-CuentaText.addEventListener('click', tabCuenta);
+$PreferenciasText.addEventListener('click', tabPreference);
+$CuentaText.addEventListener('click', tabCuenta);
+$ShowAccounts.addEventListener('click', MostrarPerfiles);
+
+
+
 
 function tabPreference() {
-    const isClosed = CuentaTab.classList.contains('inactive');
+    const isClosed = $CuentaTab.classList.contains('inactive');
     if(!isClosed){
-        CuentaTab.classList.add('inactive');
+        $CuentaTab.classList.add('inactive');
     }
-    PreferenceTab.classList.toggle('inactive');
+    $PreferenceTab.classList.toggle('inactive');
 }
 
 function tabCuenta() {
-    const isClosed = PreferenceTab.classList.contains('inactive');
+    const isClosed = $PreferenceTab.classList.contains('inactive');
     if(!isClosed){
-        PreferenceTab.classList.add('inactive');
+        $PreferenceTab.classList.add('inactive');
     }
 
-    CuentaTab.classList.toggle('inactive');
-    
+    $CuentaTab.classList.toggle('inactive');
 }
+
+function MostrarPerfiles(){
+    $Accounts.classList.toggle('inactive');
+}
+
+const Cuentas_perfiles = [];
+Cuentas_perfiles.push({
+    nombre: 'Diana De la concha',
+    correo: 'dianadelaconcha77@gmail.com',
+});
+Cuentas_perfiles.push({
+    nombre: 'Logan 07',
+    correo: 'logandiazdelaconcha@gmail.com',
+});
+
+
+function Perfiles(arr){
+    for (profile of arr){
+        const Profile = document.createElement('button');
+        Profile.classList.add('profiles');
+
+        const texto = document.createElement('p');
+        texto.innerText = profile.nombre;
+        const text = document.createElement('p');
+        text.innerText = profile.correo;
+
+
+        Profile.appendChild(texto);
+        Profile.appendChild(text);
+        $Cuentas.appendChild(Profile);
+    }
+}
+Perfiles(Cuentas_perfiles);
+
 
